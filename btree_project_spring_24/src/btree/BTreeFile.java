b4f2d237ac4dd4ab03da074561d404f5c05e85d4
Muna Bhattarai <112001777+bhattaraimuna@users.noreply.github.com>
2024-02-20 19:29:08 -0600

    Update BTreeFile.java

--- btree_project_spring_24/src/btree/BTreeFile.java
+++ btree_project_spring_24/src/btree/BTreeFile.java
@@ -902,6 +902,9 @@ public class BTreeFile extends IndexFile implements GlobalConst {
 						unpinPage(leafPage.getCurPage(),true);// unpin the leafPage as the recorded is deleted and now the leafPage is not needed 
 						System.out.println("Instance of Record "+key+" deleted successfully");
 					}
+					else {
+						System.out.pritln("All Instances of Record "+key+" are deleted");
+					}
 						
 				}
 			}
